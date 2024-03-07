687ff8472f3662aec2fcaa7b72766faac0e61cc1
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2024-03-07 17:02:04 +0900

    Initial Commit

---
+++ Learn_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learn_SwiftUI
+//
+//  Created by hiroto taniguchi on 2024/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learn_SwiftUI/Learn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUIApp.swift
+//  Learn_SwiftUI
+//
+//  Created by hiroto taniguchi on 2024/03/07.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
