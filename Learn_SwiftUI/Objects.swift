75e17a498b08ed264fe10af225f0e4ea905fc291
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2024-03-07 18:30:04 +0900

    Init Commit & Learn Objects & Modifire

---
+++ Learn_SwiftUI/Objects.swift
@@ -0,0 +1,18 @@
+//
+//  Objects.swift
+//  Learn_SwiftUI
+//
+//  Created by hiroto taniguchi on 2024/03/07.
+//
+
+import SwiftUI
+
+struct Objects: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Objects()
+}
